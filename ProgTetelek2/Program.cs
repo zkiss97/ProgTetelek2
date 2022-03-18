1148d4e3b27eb5828eb6fbbec1e591dfd2714f8e
zkiss97 <zkiss1997@gmail.com>
2022-03-18 11:40:33 +0100

    Add project files.

---
+++ ProgTetelek2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgTetelek2
+{
+    class Courage
+    {
+        public int hossz;
+        public int[] tomb = new int[10];
+        private int hanyadikvagyok;
+        private int nagyobbvagye;
+        private string jel;
+        public int bennevane;
+    }
+        class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
